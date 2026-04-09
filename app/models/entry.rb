c604b8a743008c0aee2b37d71f87a785aed1faa0
Drew Hund <languageinvestigator@gmail.com>
2026-04-09 14:16:16 -0400

    new entry form and validations

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,14 @@
 class Entry < ApplicationRecord
   belongs_to :user
+
+  validates :name, :username, :password, presence: true
+  validate :url_format
+
+  private
+
+  def url_format
+    unless url.include?("http" || "https")
+      errors.add(:url, "must be valid")
+    end
+  end
 end
