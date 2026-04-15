01821a15d7214152c8f68625ca8c719d349efa50
Drew Hund <languageinvestigator@gmail.com>
2026-04-15 15:51:30 -0400

    db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,57 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Entry.destroy_all
+
+Entry.create!(
+  user_id: User.first.id,
+  name: "GitHub",
+  url: "https://github.com",
+  username: "username",
+  password: "123456"
+)
+
+Entry.create!(
+  user_id: User.first.id,
+  name: "Dribbble",
+  url: "https://dribbble.com",
+  username: "username",
+  password: "123456"
+)
+
+Entry.create!(
+  user_id: User.first.id,
+  name: "StackOverflow",
+  url: "https://stackoverflow.com",
+  username: "username",
+  password: "123456"
+)
+
+Entry.create!(
+  user_id: User.first.id,
+  name: "Amazon",
+  url: "https://amazon.com",
+  username: "username",
+  password: "123456"
+)
+
+Entry.create!(
+  user_id: User.first.id,
+  name: "X",
+  url: "https://x.com",
+  username: "username",
+  password: "123456"
+)
+
+Entry.create!(
+  user_id: User.first.id,
+  name: "Facebook",
+  url: "https://facebook.com",
+  username: "username",
+  password: "123456"
+)
+
+Entry.create!(
+  user_id: User.first.id,
+  name: "Udemy",
+  url: "https://udemy.com",
+  username: "username",
+  password: "123456"
+)
